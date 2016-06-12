4cc8d9c3ab6f7b9ee5b15e9b5fcd330189201f1e
Ben Gollmer <jatoben@github.com>
2016-06-12 12:59:53 -0600

    Fix read snapshot test

--- Tests/RocksDB/RocksDBTests.swift
+++ Tests/RocksDB/RocksDBTests.swift
@@ -152,15 +152,16 @@ class RocksDBTests: XCTestCase {
 
   func testReadSnapshot() {
     do {
-      try db.put("foo", value: "bar")
+      try db.put("foo-snap", value: "bar")
+      try db.delete("baz-snap")
       let snap = db.createReadSnapshot()
-      try db.put("baz", value: "quux")
+      try db.put("baz-snap", value: "quux")
 
       let readOpts = DBReadOptions()
       readOpts.setReadSnapshot(snap)
-      XCTAssertEqual(try db.get("foo", options: readOpts) as String?, "bar", "Didn't read expected value with read snapshot")
-      XCTAssertNil(try db.get("baz", options: readOpts) as String?, "Read unexpected value from read snapshot")
-      XCTAssertEqual(try db.get("baz") as String?, "quux", "Didn't read expected value without read snapshot")
+      XCTAssertEqual(try db.get("foo-snap", options: readOpts) as String?, "bar", "Didn't read expected value with read snapshot")
+      XCTAssertNil(try db.get("baz-snap", options: readOpts) as String?, "Read unexpected value from read snapshot")
+      XCTAssertEqual(try db.get("baz-snap") as String?, "quux", "Didn't read expected value without read snapshot")
     } catch {
       XCTFail("\(error)")
     }
