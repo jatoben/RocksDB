28bd5f256d041aaa8b0431d307ae1061a52400ca
Ben Gollmer <jatoben@github.com>
2016-06-18 19:17:45 -0600

    Only need shared lib

--- install-rocksdb-master.sh
+++ install-rocksdb-master.sh
@@ -3,8 +3,8 @@ set -ev
 
 echo "Installing RocksDB"
 curl -s -L -O "https://github.com/facebook/rocksdb/archive/master.zip"
-unzip master.zip
+unzip -q master.zip
 cd rocksdb-master
 make shared_lib
-sudo make install
+sudo make install-shared
 cd ..
